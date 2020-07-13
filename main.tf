c4b879adcdcdf786b6b86ccbdd84b290f0a02719
Akshay Namdev <aknamdev05@gmail.com>
2020-07-13 18:51:52 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  access_key = "AKIAJ2WKQHNT4L7UEMVQ"
+  secret_key = "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"
+  region     = "us-east-1"
+}
+resource "aws_instance" "Github" {
+  ami           = "ami-0323c3dd2da7fb37d"
+  instance_type = "t2.micro"
+  key_name = "N_virginia"
+  tags = {
+    Name = "Github-enterprise {{timestamp}}"
+  }
+ }
+
+data "aws_vpc" "selected" {
+  id = "${var.vpc_id}"
+}
+
+data "aws_subnet" "subnet-b66b9ce9" {
+  vpc_id            = "${data.aws_vpc.selected.id}"
+  availability_zone = "us-east-1a"
+}
+
+
+data "aws_security_group" "selected" {
+  filter {
+      name = "Github"
+      values = ["sg-0c98bd6fb48255ffc"]
+  }
+  vpc_id = "${data.aws_vpc.selected.id}"
+}
