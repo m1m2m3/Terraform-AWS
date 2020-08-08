0f88c394388a5a7bdf059c16a5ccb3e1af989893
Akshay Namdev <aknamdev05@gmail.com>
2020-08-08 13:01:18 +0530

    Update var.tf

--- terraform-create-VM-with specific VPC/var.tf
+++ terraform-create-VM-with specific VPC/var.tf
@@ -1,8 +1,34 @@
 variable "instance_count" {
   description = "Number of instances to launch"
   type        = number
-  default     = 1
+  default     = 2
 }
+variable "aws_ami_id" {
+  default="ami-02354e95b39ca8dec"
+}
+
+variable "instance_type" {
+  default="t2.micro"
+}
+variable "availability_zone" {
+  default="us-east-1a"
+}
+
+variable "ec2_device_names" {
+  default = [
+    "/dev/sdd",
+    "/dev/sde",
+    "/dev/sdf",
+  ]
+}
+variable "ec2_ebs_volume_count" {
+  default = 1
+}
+
+variable "ec2_ebs_volume_size" {
+  default = 1
+}
+
 variable "vpc_id" {
   default="vpc-00ea3668106f9d79c"
 }
